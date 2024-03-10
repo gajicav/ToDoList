7e11e879fc3cb32b8c446f7b1951d08a904f574b
gajicav <gajicav@mail.uc.edu>
2024-03-09 23:21:07 -0500

    Add project files.

---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Text;
+using SQLite;
+namespace ToDoList.Models
+{
+    internal class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool Done { get; set; }
+    }
+}
---
+++ ToDoList/Views/ToDoListPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Data;
+using ToDoList.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace ToDoList.Views
+{
+    public partial class ToDoListPage : ContentPage
+    {
+        public ToDoListPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            //TodoItemDatabase database = await TodoItemDatabase.Instance;
+            listView.ItemsSource = await App.Database.GetItemsAsync();
+        }
+        async void OnItemAdded(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TodoItemPage
+            {
+                BindingContext = new TodoItem()
+            });
+        }
+        async void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new TodoItemPage
+                {
+                    BindingContext = e.SelectedItem as TodoItem
+                });
+            }
+        }
+    }
+}
